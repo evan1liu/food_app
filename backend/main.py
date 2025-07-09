1d46aac986a9ec106fc9c32ae77207caf333ba6c
Evan Liu <evan20050827@gmail.com>
2025-07-09 14:22:59 -0500

    template for the python-svelte stack

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,63 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+class MealCategoryRequest(BaseModel):
+    meal_category: str
+
+@app.post("/meal_category")
+async def print_meal_category(request: MealCategoryRequest):
+    print(request.meal_category)
+    
+    return f"{request.meal_category} successfully printed"
+
+class OnboardingRequest(BaseModel):
+    time_to_cook_per_day: float
+    money_to_spend_per_week: float
+    allergies: list[str]
+    nutrition_goals: list[str]
+    food_restrictions: list[str]
+    food_preferences: dict[str, str]
+    address: str # there could be a type for address
+    
+
+# @onboarding_router.post("/onboarding")
+# async def onboarding_user():
+    
+#     await user_collection.update_one(
+#         {"email": current_user.email},
+#         {"$set": {
+#             "major": user_data.major, 
+#             "entry_year": user_data.entry_year,
+#             "full_name": user_data.full_name,
+#             "onboarded": True,
+#             # Store hierarchical info for future use
+#             "program_info": selected_program_info
+#         }}
+#     )
+
+#     # Initialize a roadmap during onboarding with hierarchical info
+#     await initialize_full_roadmap(
+#         current_user.id, 
+#         user_data.major, 
+#         user_data.entry_year,
+#         selected_program_info
+#     )
+#     return {"message": "User updated successfully"}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,28 @@
+<script lang="ts">
+    const meal_categories = ["Mexican", "Chinese", "Indian"]
+
+    async function logCategoryInConsole(meal_category: string) {
+        console.log(meal_category)
+
+        const response = await fetch(`http://127.0.0.1:8000/meal_category`, {
+				method: 'POST',
+                headers: {
+						'Content-Type': 'application/json'
+					},
+				body: JSON.stringify({ meal_category: meal_category })
+			});
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<a href="/meals"><h1>Go to Meal Page</h1></a>
+
+{#each meal_categories as meal_category}
+    <button onclick={() => logCategoryInConsole(meal_category)}>{meal_category} Food</button>
+{/each}
+
+<style>
+
+</style>
