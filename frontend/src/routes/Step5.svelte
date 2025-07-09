295bd75ff906b43f422142b39fe19d26018e4c03
Ashmit2520 <105515137+Ashmit2520@users.noreply.github.com>
2025-07-09 15:11:17 -0500

    Made StepX.svelte files

---
+++ frontend/src/routes/Step5.svelte
@@ -0,0 +1,31 @@
+<script>
+    /** @type {string[]} */
+    export let selectedAllergies = [];
+    const allergies = [
+      "Peanuts", "Tree nuts", "Milk", "Eggs", "Fish", "Shellfish",
+      "Wheat", "Soy", "Sesame", "Gluten", "Mustard", "Celery",
+      "Sulfites", "Lupin", "Molluscs"
+    ];
+  
+    /**
+     * @param {string} allergy
+     */
+    function toggleAllergy(allergy) {
+      if (selectedAllergies.includes(allergy)) {
+        selectedAllergies = selectedAllergies.filter(a => a !== allergy);
+      } else {
+        selectedAllergies = [...selectedAllergies, allergy];
+      }
+    }
+  </script>
+  
+  <h2>Select your allergies:</h2>
+  {#each allergies as allergy}
+    <label>
+      <input type="checkbox" bind:group={selectedAllergies} value={allergy} />
+      {allergy}
+    </label>
+  {/each}
+  
+  <button on:click={() => gotoNextStep(selectedAllergies)}>Next</button>
+  
