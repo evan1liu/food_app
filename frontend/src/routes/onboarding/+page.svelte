0c91d9c0ce938c7032cb68189c658f00e0958ef3
Evan Liu <evan20050827@gmail.com>
2025-07-09 16:02:00 -0500

    updated imports

--- frontend/src/routes/onboarding/+page.svelte
+++ frontend/src/routes/onboarding/+page.svelte
@@ -1,12 +1,12 @@
 <script>
-    import Step1 from "../Step1.svelte";
-    import Step2 from "../Step2.svelte";
-    import Step3 from "../Step3.svelte";
-    import Step4 from "../Step4.svelte";
-    import Step5 from "../Step5.svelte";
-    import Step6 from "../Step6.svelte";
-    import Step7 from "../Step7.svelte";
-    
+    import Step1 from "./Step1.svelte";
+    import Step2 from "./Step2.svelte";
+    import Step3 from "./Step3.svelte";
+    import Step4 from "./Step4.svelte";
+    import Step5 from "./Step5.svelte";
+    import Step6 from "./Step6.svelte";
+    import Step7 from "./Step7.svelte";
+
         // Example function to submit onboarding data
     async function submitOnboarding(onboardingData) {
         try {
@@ -31,3 +31,5 @@
         }
     }
 </script>
+
+<Step1 />
